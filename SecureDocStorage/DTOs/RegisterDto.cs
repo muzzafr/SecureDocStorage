2aa7dfa0928ee3c3e5e7dbb46668deb31695d8a9
muzzafr <muzzafrhussain@outlook.com>
2025-05-21 07:59:29 +0100

    Add DTOs for Register and Login requests

---
+++ SecureDocStorage/DTOs/RegisterDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SecureDocStorage.DTOs
+{
+    public class RegisterDto
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
