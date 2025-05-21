a640292ac3ba3db2587a3e2bc93141f967db7a1d
muzzafr <muzzafrhussain@outlook.com>
2025-05-21 07:50:12 +0100

    Add JWT token generation service

---
+++ SecureDocStorage/Services/JwtService.cs
@@ -0,0 +1,37 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace SecureDocStorage.Services
+{
+    public class JwtService
+    {
+        private readonly string _secret;
+        
+        public JwtService(IConfiguration config)
+        {
+            _secret = config["Jwt:Key"] ?? "ThisIsASecretKeyForJWTDonotUseInProduction";
+
+        }
+
+        public string GenerateToken(int userId, string username)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.UTF8.GetBytes(_secret);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, userId.ToString()),
+                    new Claim(ClaimTypes.Name, username)
+                }),
+                Expires = DateTime.UtcNow.AddHours(2),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
