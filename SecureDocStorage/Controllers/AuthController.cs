870227887e4dc1927ea48a22a3ab06fa947601a0
muzzafr <muzzafrhussain@outlook.com>
2025-05-21 09:49:16 +0100

    Implement AuthController with Register and Login endpoints

---
+++ SecureDocStorage/Controllers/AuthController.cs
@@ -0,0 +1,69 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SecureDocStorage.Data;
+using SecureDocStorage.DTOs;
+using SecureDocStorage.Models;
+using SecureDocStorage.Services;
+
+namespace SecureDocStorage.Controllers
+{
+    [ApiController]
+    [Route("auth")]
+    public class AuthController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly JwtService _jwtService;
+
+        public AuthController(ApplicationDbContext context, JwtService jwtService)
+        {
+            _context = context;
+            _jwtService = jwtService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDto dto)
+        {
+            if (await _context.Users.AnyAsync(u => u.UserName == dto.Username))
+                return BadRequest("Username already exists");
+            CreatePasswordHash(dto.Password, out byte[] hash, out byte[] salt);
+
+            var user = new User
+            {
+                UserName = dto.Username,
+                PasswordHash = hash,
+                PasswordSalt = salt
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return Ok("User registerd.");
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login (LoginDto dto)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.UserName == dto.Username);
+            if (user == null || !VerifyPassword(dto.Password, user.PasswordHash, user.PasswordSalt))
+                return Unauthorized("Invalid Credentials");
+
+            var token = _jwtService.GenerateToken(user.Id, user.UserName);
+            return Ok(new { token });
+        }
+
+        private void CreatePasswordHash( string password, out byte[] hash, out byte[] salt)
+        {
+            using var hmac = new HMACSHA256();
+            salt = hmac.Key;
+            hash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+        }
+
+        private bool VerifyPassword(string password, byte[] hash, byte[] salt)
+        {
+            using var hmac = new HMACSHA256();
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            return computedHash.SequenceEqual(hash);
+        }
+    }
+}
